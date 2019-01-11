5ad0ec40d4837f4742181e5b1fedc2a681d33028
Evgeniy Beletskiy <mbgroup1988@gmail.com>
2019-01-11 09:14:00 +0300

    add conf file

---
+++ conf-assert.js
@@ -0,0 +1,26 @@
+exports.config = {
+  directConnect: true,
+  
+  // The address of a running selenium server. 
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {
+      args: ['-start-maximized'] 
+    }
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['assertion.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 60000
+  },
+};
